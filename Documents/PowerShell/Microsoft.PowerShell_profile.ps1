57867e265c88502ca6d04d8f6b36939b10efa81d
techstay <lovery521@gmail.com>
2024-10-23 14:04:26 +0800

    prepare new pc

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,6 +1,7 @@
 # ------------------------------------------------------------------------------
 # Import Modules
 # ------------------------------------------------------------------------------
+# scoop intall posh-git scoop-completion terminal-icons
 # Import-Module posh-git
 # Import-Module -Name Terminal-Icons
 # Import-Module "$($(Get-Item $(Get-Command scoop.ps1).Path).Directory.Parent.FullName)\modules\scoop-completion"
@@ -8,22 +9,24 @@
 # ------------------------------------------------------------------------------
 # Completions
 # ------------------------------------------------------------------------------
-# Get-ChildItem -Path $PROFILE/../Completions/ | ForEach-Object { . $_ }
+# gh completion -s powershell | Out-String | Invoke-Expression
+hugo completion powershell | Out-String | Invoke-Expression
+# genact --print-completions powershell | Out-String | Invoke-Expression
 
 # ------------------------------------------------------------------------------
 # Terminal Configuration
 # ------------------------------------------------------------------------------
 
-Invoke-Expression (&starship init powershell)
-# oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/montys.omp.json" | Invoke-Expression
+# Invoke-Expression (&starship init powershell)
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/1_shell.omp.json" | Invoke-Expression
 
-# set utf-8 encoding
+# Set up utf-8 encoding
 $OutputEncoding = [console]::InputEncoding = [console]::OutputEncoding = New-Object System.Text.UTF8Encoding
 
 # PSReadLine
 Set-PSReadLineOption -PredictionSource HistoryAndPlugin
 
-# z.lua
+# Set up z.lua
 Invoke-Expression (& { (lua $HOME/z.lua/z.lua --init powershell enhanced) -join "`n" })
 
 function zb {
@@ -39,4 +42,11 @@ function dot {
     git --git-dir="$HOME/.dotfiles/" --work-tree=$HOME $args
 }
 
-Set-Alias winfetch pwshfetch-test-1
+function scoop-clean {
+    scoop cleanup *
+    scoop cache rm *
+}
+#f45873b3-b655-43a6-b217-97c00aa0db58 PowerToys CommandNotFound module
+
+Import-Module -Name Microsoft.WinGet.CommandNotFound
+#f45873b3-b655-43a6-b217-97c00aa0db58
